6874bcf4e7765dd30c718befca2312282f03dcec
Ian Webster <iwebster@google.com>
2014-07-07 15:46:07 -0700

    Fix loading

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,14 @@
+console.log('poop');
 chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+  var readyStateCheckInterval = setInterval(function() {
+  if (document.readyState === "complete") {
+    clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+    // ----------------------------------------------------------
+    // This part of the script triggers when page is done loading
+    console.log("Hello. This message was sent from scripts/inject.js");
+    // ----------------------------------------------------------
 
-	}
-	}, 10);
+  }
+  }, 10);
 });
