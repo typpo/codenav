7e62e237f80f9280f7111f923d4332c21566a46a
Ian Webster <iwebster@google.com>
2014-07-08 13:14:56 -0700

    bump versions..

--- package_firefox.sh
+++ package_firefox.sh
@@ -5,6 +5,7 @@ pushd `dirname $0`
 pushd firefox
 #cfx xpi --update-link http://ianww.com/codenav/firefox_latest --update-url http://ianww.com/codenav/firefox_update_rdf
 cfx xpi
+mv codenav.xpi ..
 popd
 
 popd
