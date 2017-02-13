ea0f91cc587598d2f911792ea87a3d1f276e4b46
Ian Webster <ianw_git@ianww.com>
2017-02-12 23:14:13 -0800

    update firefox packaging

--- package_firefox.sh
+++ package_firefox.sh
@@ -5,9 +5,8 @@ pushd `dirname $0`
 ./setup_hardlinks.sh
 
 pushd firefox
-#cfx xpi --update-link http://ianww.com/codenav/firefox_latest --update-url http://ianww.com/codenav/firefox_update_rdf
-cfx xpi
-mv codenav.xpi ..
+jpm xpi
+mv *.xpi ../codenav.xpi
 popd
 
 popd
